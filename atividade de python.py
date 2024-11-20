707f9afb277cbdb824b786a61d3ba54d8c1e650b
duda61 <mariaeduarda071213@gmail.com>
2024-11-20 14:16:32 -0300

    Add files via upload

---
+++ atividade de python.py
@@ -0,0 +1,13 @@
+
+contato = {}
+
+nome = input("Digite o nome do contato: ")
+telefone = input("Digite o número de telefone do contato: ")
+email = input("Digite o email do contato: ")
+
+contato["nome"] = nome
+contato["telefone"] = telefone
+contato["email"] = email
+
+print("\nAs informações do contato são:")
+print(contato)
